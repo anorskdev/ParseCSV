96cf2a5741b3da93a27cfd7c12cc35e3fbd223e5
anorskdev <anorskdev@gmail.com>
2023-10-30 11:32:20 -0600

    Initial Commit

---
+++ ParseCSV/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ParseCSV
+//
+//  Created by Allen Norskog on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ParseCSV/ParseCSVApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParseCSVApp.swift
+//  ParseCSV
+//
+//  Created by Allen Norskog on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ParseCSVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
